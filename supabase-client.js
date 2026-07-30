cfdf01c17e3b7f688dd8b49537b84bfe5250fea8
ElcraftWorld <jackie.crockett1@yahoo.com>
2026-07-30 07:30:15 -0400

    Update supabase-client.js

--- supabase-client.js
+++ supabase-client.js
@@ -1,8 +1,11 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm';
+import { createClient } from
+  "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
 
-const SUPABASE_URL = 'https://qyscjpbcltlserrdkruz.supabase.co';
+const SUPABASE_URL =
+  "https://qyscjpbcltlserrdkruz.supabase.co";
 
-const SUPABASE_PUBLISHABLE_KEY = 'sb_publishable_rxMz0MPjI16b1cXND8uQug_4A7wJMk1';
+const SUPABASE_PUBLISHABLE_KEY =
+  "sb_publishable_rxMz0MPjI16b1cXND8uQug_4A7wJMk1";
 
 export const supabase = createClient(
   SUPABASE_URL,
